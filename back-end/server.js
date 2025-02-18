543d26cb5cf086f2d9ac1b41230d24960758596d
Pijus-B <p.bogusis@gmail.com>
2025-02-18 21:06:28 +0200

    some stuff

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get("/" ,(req, res) => {
+    res.send("Ar tu esi tam pasiruoses?");
+});
+
 app.listen(5001, () => {
     console.log('Server started at http://localhost:5001');
 });
