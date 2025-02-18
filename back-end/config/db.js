4569ea70d60eaa314c515561c64379b3bf0d22b8
Pijus-B <p.bogusis@gmail.com>
2025-02-18 21:47:56 +0200

    api and models

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,8 +1,8 @@
-import mangoose from "mangoose";
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mangoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }
     catch (error){
